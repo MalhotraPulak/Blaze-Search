edb40067a147b60e4a639a15612a5db730d88511
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-09-06 14:42:33 +0530

    updated run file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-time python3 parser.py ../minidump.xml ../output ../stats.txt > ../dump.txt
-time python3 search.py output "c:Zambian"
-time python3 search.py output/ "l:veikkausliiga"
-time python3 search.py output  "stephen kunda"
-time python3 search.py output "r:Imdb"
-time python3 search.py output "Pop"
+time python3 2019101050/parser.py wiki.xml output/ stats.txt > dump.txt
+# time python3 search.py output "c:Zambian"
+# time python3 search.py output/ "l:veikkausliiga"
+# time python3 search.py output  "stephen kunda"
+# time python3 search.py output "r:Imdb"
+# time python3 search.py output "Pop"
