0d1ce87150e9b349738531a57d6d5d3532e82b73
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-09-07 15:48:27 +0530

    writing new search

--- 2019101050/search.py
+++ 2019101050/search.py
@@ -4,8 +4,7 @@ from copy import deepcopy
 from nltk.stem import SnowballStemmer
 
 snowball_stemmer = SnowballStemmer('english')
-lines = []
-dic = {}
+WEIGHTS = [5, 3, 1, 2, 1, 1]
 
 
 def stem(token):
@@ -20,64 +19,111 @@ field_map = {
         't':5
 };
 
+def process_word(word, field):
+    index_file_loc = f"mergedIndex/{word[0]}.txt"
+    file = open(index_file_loc, "r")
+    lines = file.readlines()
+    dic = {}
+    tokens = []
+    for line in lines:
+        tokens = line.split(';')
+        if tokens[0] != word:
+            continue 
+        else:
+            break
+    token = word
+    for segment in tokens[1:]:
+        segment = segment.strip()
+        doc_id_freq = segment.split(":")
+        doc_id = int(doc_id_freq[0], base=16)
+        freq_str = str(doc_id_freq[1]) + 'z'
+        last = ''
+        freq = 0
+        for c in freq_str:
+            if c in list(field_map.keys()) + ['p', 'q']:
+                if c == 'p':
+                    dic[token][2].append([doc_id, 1])
+                elif c == 'q':
+                    dic[token][1].append([doc_id, 1])
+                if freq != 0 and last:
+                    dic[token][field_map[last]].append([doc_id, freq])
+                    freq = 0
+                last = c
+            else:
+                freq = freq * 10 + int(c) 
+  
+
+
 
 def process_query(query):
-    global dic
     units = query.split()
+    # TODO translate text to remove punctuation
     # print(units)
-    tokens = []
     for unit in units:
-        if len(unit) >= 3:
-            if unit[1] == ':':
-                unit = unit[2:]
-        tokens.append(unit)
+        field = -1
+        if len(unit) >= 3 and unit[1] == ":":
+            tok = unit[0]
+            if tok == 't':
+                field = 0
+            elif tok == 'i':
+                field = 1
+            elif tok == 'b':
+                field = 2
+            elif tok == 'c':
+                field = 3
+            elif tok == 'r':
+                field = 4
+            elif tok == 'l':
+                field = 5
+            unit = unit[2:]
+        process_word(stem(unit.lower()), field) 
     # print(tokens)
-    ans = {}
-    for token in tokens:
-        # title: 0, info: 1, body: 2, cat: 3, ref: 4, links: 5
-        og = deepcopy(token)
-        token = stem(token.lower())
-        for line in lines:
-            segments = line.split(';')
-            if segments[0] != token:
-                continue
-            dic[token] = [[], [], [], [], [], []]
-            for segment in segments[1:]:
-                segment = segment.strip()
-                doc_id_freq = segment.split(":")
-                doc_id = int(doc_id_freq[0], base=16)
-                freq_str = str(doc_id_freq[1]) + 'z'
-                last = ''
-                freq = 0
-                print(doc_id, freq_str)
-                for c in freq_str:
-                    if c in list(field_map.keys()) + ['p', 'q']:
-                        if c == 'p':
-                            dic[token][2].append([doc_id, 1])
-                        elif c == 'q':
-                            dic[token][1].append([doc_id, 1])
-                        if freq != 0 and last:
-                            dic[token][field_map[last]].append([doc_id, freq])
-                            freq = 0
-                        last = c
-                    else:
-                        freq = freq * 10 + int(c) 
-            break
-        ans[og] = {}
-        if token in dic:
-            dic_token = dic[token]
-        else:
-            dic_token = [[], [], [], [], [], []]
-        ans[og]["title"] = dic_token[0]
-        ans[og]["body"] = dic_token[2]
-        ans[og]["infobox"] = dic_token[1]
-        ans[og]["categories"] = dic_token[3]
-        ans[og]["references"] = dic_token[4]
-        ans[og]["links"] = dic_token[5]
-        for key, value in ans[og].items():
-            if not value:
-                ans[og][key] = ["No Doc Found"]
-    pprint.pprint(ans, indent=1)
+    # ans = {}
+    # for token in tokens:
+    #     # title: 0, info: 1, body: 2, cat: 3, ref: 4, links: 5
+    #     og = deepcopy(token)
+    #     token = stem(token.lower())
+    #     for line in lines:
+    #         segments = line.split(';')
+    #         if segments[0] != token:
+    #             continue
+    #         dic[token] = [[], [], [], [], [], []]
+    #         for segment in segments[1:]:
+    #             segment = segment.strip()
+    #             doc_id_freq = segment.split(":")
+    #             doc_id = int(doc_id_freq[0], base=16)
+    #             freq_str = str(doc_id_freq[1]) + 'z'
+    #             last = ''
+    #             freq = 0
+    #             print(doc_id, freq_str)
+    #             for c in freq_str:
+    #                 if c in list(field_map.keys()) + ['p', 'q']:
+    #                     if c == 'p':
+    #                         dic[token][2].append([doc_id, 1])
+    #                     elif c == 'q':
+    #                         dic[token][1].append([doc_id, 1])
+    #                     if freq != 0 and last:
+    #                         dic[token][field_map[last]].append([doc_id, freq])
+    #                         freq = 0
+    #                     last = c
+    #                 else:
+    #                     freq = freq * 10 + int(c) 
+    #         break
+    #     ans[og] = {}
+    #     if token in dic:
+    #         dic_token = dic[token]
+    #     else:
+    #         dic_token = [[], [], [], [], [], []]
+    #     ans[og]["title"] = dic_token[0]
+    #     ans[og]["body"] = dic_token[2]
+    #     ans[og]["infobox"] = dic_token[1]
+    #     ans[og]["categories"] = dic_token[3]
+    #     ans[og]["references"] = dic_token[4]
+    #     ans[og]["links"] = dic_token[5]
+    #     for key, value in ans[og].items():
+    #         if not value:
+    #             ans[og][key] = ["No Doc Found"]
+    # pprint.pprint(ans, indent=1)
 
 
 def main():
@@ -88,8 +134,6 @@ def main():
     try:
         if index_loc[-1] == '/':
             index_loc = index_loc[: -1]
-        with open(f"{index_loc}/index.txt", "r") as file:
-            lines = file.readlines()
         process_query(query_string)
         print()
     except Exception as e:
